62654e862dbe68f3c7f9330b2c3da7e81b43cad0
seba@sebastiaanprovost.com <seba@stekkz.com>
2016-01-18 10:32:03 +0100

    Some bugfix for the alias command + making it interactive

--- src/Commands/ExtAlias.php
+++ src/Commands/ExtAlias.php
@@ -16,7 +16,7 @@ class ExtAlias extends Command
 	 *
 	 * @var string
 	 */
-	protected $name = 'app:alias {alias}';
+	protected $name = 'app:alias';
 
 	/**
 	 * The console command description.
@@ -42,43 +42,49 @@ class ExtAlias extends Command
 	 */
 	public function handle()
 	{
-    $config_file = config_path() . '/app.php';
 
-    if(!file_exists($config_file))
-    {
-      return $this->error('The app.php configuration file is missing.');
-    }
+		$config_file = config_path() . '/app.php';
 
-    $config_file_data = file_get_contents($config_file);
-    $artisan_ext_head =  '        /*' . "\n" . '         * Artisan Extended added aliases...' . "\n" . '         */' . "\n"; //78
+		if(!file_exists($config_file))
+		{
+			return $this->error('The app.php configuration file is missing.');
+		}
 
-    if(strpos($config_file_data, $artisan_ext_head) == false)
-    {
-      $pos = strpos($config_file_data, '\'aliases\' => [');
-      $config_file_data_new = substr_replace($config_file_data, "\n" . $artisan_ext_head . '        ' . $this->argument('alias') . '::class,' . "\n" , $pos + 17, 0);
-      file_put_contents($config_file, $config_file_data_new);
-    }
-    else
-    {
-      $pos = strpos($config_file_data, $artisan_ext_head);
-      $config_file_data_new = substr_replace($config_file_data, '        ' . $this->argument('alias') . '::class,' . "\n", $pos + 68, 0);
-    }
+		$alias_name = $this->ask('What is the name for your alias?');
+		$alias_path = $this->ask('what is the path for your alias?');
 
-    file_put_contents($config_file_data, $config_file_data_new);
-    return $this->info('The following alias has been succesfully added: ' . $this->argument('alias'));
+		if (str_contains($alias_path, '"'))
+		{
+			return $this->error('You have to enclose the path in " (double quotes) to make sure that the backslashes are correctly saved.');
+		}
 
-  }
+		$alias_line = '        ' . '\'' . $alias_name . '\'	=>	' . $alias_path . '::class,';
+
+		$config_file_data = file_get_contents($config_file);
+		$artisan_ext_head =  '        /*' . "\n" . '         * Artisan Extended added aliases...' . "\n" . '         */' . "\n"; //78
+
+		if(strpos($config_file_data, $artisan_ext_head) == false)
+		{
+			$pos = strpos($config_file_data, '\'aliases\' => [');
+			$config_file_data_new = substr_replace($config_file_data, "\n" . $artisan_ext_head . $alias_line . "\n" , $pos + 17, 0);
+		}
+		else
+		{
+			$pos = strpos($config_file_data, $artisan_ext_head);
+			$config_file_data_new = substr_replace($config_file_data, $alias_line . "\n", $pos + 68, 0);
+		}
+
+		file_put_contents($config_file, $config_file_data_new);
+
+		return $this->info('The following alias has been succesfully added: ' . $alias_path);
+
+	}
 
 	/**
 	 * Get the console command arguments.
 	 *
 	 * @return array
 	 */
-	protected function getArguments()
-	{
-		return array(
-			array('alias', InputArgument::REQUIRED, 'The alias definition'),
-		);
-	}
+
 
 }
